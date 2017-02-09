1ac29a3e8fd236a087305ad7b83c61ae66852c50
Jose Manuel (Ito) <ito-matsuda@hotmail.com>
2017-02-09 11:42:22 -0500

    tadaaaa, it's finished

--- a1.m
+++ a1.m
@@ -1,6 +1,6 @@
 % Written by Jose Manuel Matsuda 10150406
 % I verify that this is of my own work 
-function [Bspace, Nspace] = a1(A,b)
+function [Bspace, Nspace] = a1(A,b);
 %Augment the matrix with the result vector
 augmented = [A,b];
 %Find RREF
@@ -49,51 +49,34 @@ end
 disp('Bspace is')
 disp(Bspace)
 % Begin finding null space, useful link
-% http://math.stackexchange.com/questions/598508/basis-of-a-null-space-of-a-matrix
-% When finding the Null Space, ALWAYS reduce to Reduced Row Echelon Form (RREF). From There, it is just 
-% finding the solution to Ax = 0. where x and zero are vectors.
-
-% Or use this link http://math.stackexchange.com/questions/88301/finding-the-basis-of-a-null-space
-% Augment A with zero vector then reduce to RREF
-zeroVector = zeros(sizeOfA(1),1); % Make the zero vector using the height of 'A'
-augmentZero = [A,zeroVector];
-rrefAugZero = rref(augmentZero);
-Nspace = zeros(sizeOfA(1),1); % Instantiate the thing to be used later
-
-% Another way of doing this is to just take the rref of A, look for leading
-% entries, any zero rows it doesnt matter what you put there (so say 1)
-% for the ones that are have a one as the leading co -eff, take the
-% negative of the other value (likely in the very last column)
 % Look at http://www.math.odu.edu/~bogacki/cgi-bin/lat.cgi?c=null
+% My way of finding a basis of the null space of the matrix is based off
+% patterns found in the link above
+% Checked with example solution given 
+
+% Step 1) Reduce matrix A to RREF
+% Step 2) The zero rows will have a solution of just 1
+% Step 3) Non-zero rows will have a solution of the negative of the 
+%         Negative value in the last column
+Nspace = zeros(sizeOfA(1),1); % Instantiate the vector to be used later
+zeroCounter = 0;
+Arref = rref(A);
+for whatRow = 1:sizeOfA(1) % Iterate through rows
+    for whatColumn = 1:sizeOfA(2)
+        if (Arref(whatRow,whatColumn) == 0)
+           zeroCounter = zeroCounter + 1;
+        end 
+    end
+    % Finish iterating through columns
+    % If the number of columns = number of zeros, then you know that the
+    % value at that row must be a '1' or anything else for that matter
+    if (zeroCounter == sizeOfA(2))
+        Nspace(whatRow) = 1;
+        zeroCounter = 0;
+    elseif (zeroCounter < sizeOfA(2)) % There was a one 
+        Nspace(whatRow) = (-1) * Arref(whatRow,whatColumn);
+        zeroCounter = 0;
+    end
+end 
 disp('Nspace is');
 disp(Nspace);
-
-
-% If you use the following code (or re-use the above one) you get Null
-% Space of zero
-% Nspace = zeros(totalSize(1),1); %make a vector to match the height of A
-% Nspace(totalSize(1)) = rrefAugZero(totalSize(1),totalSize(2));
-% finished = false;
-% currentRow = (totalSize(1)-1); % Height of matrix go "up" matrix one row
-% total = 0;
-% while (finished == 0) % ie) while false
-%     % Must now go through the current row (not including the augmented b 
-%     % up until the pivot (1) and add em up  
-%     whichColumn = (sizeOfA(2)); % Width of matrix
-%     rowFinished = false;
-%     while (rowFinished == 0)
-%         if (whichColumn == currentRow) % You hit the pivot
-%             break;
-%         else
-%             total = total + (Nspace(whichColumn)*(A(currentRow,whichColumn)));
-%             whichColumn = whichColumn-1; % Move a column space to the left
-%         end
-%         total = 0; % Reset the total
-%     end % End going through the row
-%     Nspace(currentRow) = (rrefAugZero(currentRow,totalSize(2)) - total);
-%     if (currentRow == 1) % We are at the top so cant go up further
-%         finished = true;
-%     else
-%         currentRow = currentRow - 1;
-%     end
-% end
